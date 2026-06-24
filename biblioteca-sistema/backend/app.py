90817ca18e18300c1ac7967b72732bba515ef674
Touken-byte <baronbarrerolizzyleyla@gmail.com>
2026-06-24 10:42:42 -0400

    fix: puerto dinámico para Railway

--- biblioteca-sistema/backend/app.py
+++ biblioteca-sistema/backend/app.py
@@ -1020,4 +1020,6 @@ if __name__ == '__main__':
     inicializar_base_datos()
     
     # Ejecutar aplicación
-    app.run(debug=True, host='0.0.0.0', port=5000)
+# CORRECCIÓN
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
